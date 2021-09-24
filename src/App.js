17e887f370cc9b98a60dcb1b04b109e9491ffb31
Austin Rhoads <austinrrhoads@ymail.com>
2021-09-23 23:48:54 -0500

    replaced img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import hunter_down_sheet from './images/hunter_down_test.png'
+import hunter_down_sheet from './images/hunter_down.png'
 
 function App() {
   return (
     <div className="App">
       <div id="viewport">
-        <div id="hunter_down">
-            <img alt="" src={hunter_down_sheet} />
+        <div id="hunter">
+            <img className="hd_sheet" alt="" src={hunter_down_sheet} />
         </div>
 
       </div>
