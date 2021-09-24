0e9ed8dc275bf1bed699121493df9b4b24711bb7
Austin Rhoads <austinrrhoads@ymail.com>
2021-09-23 23:38:34 -0500

    practice sprite sheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import hunter_down_sheet from './images/hunter_down_test.png'
 
 function App() {
   return (
     <div className="App">
       <div id="viewport">
+        <div id="hunter_down">
+            <img alt="" src={hunter_down_sheet} />
+        </div>
 
       </div>
     </div>
