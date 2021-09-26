58cae230ed0421126a9189a69261a03208d21f0b
Austin Rhoads <austinrrhoads@ymail.com>
2021-09-25 21:47:08 -0500

    added lots

--- src/App.js
+++ src/App.js
@@ -1,15 +1,93 @@
 
 import './App.css';
+import React from 'react';
 import hunter_down_sheet from './images/hunter_down.png'
+import bonsai from './images/bonsai_test.png'
+import blooming from './images/bloominfukian.png'
+import ar_logo_shine from './images/ar_logo_shine_one.png'
+
 
 function App() {
+
+
+
+
+function startBlooming(e){
+
+  e.target.classList.add("blooming")
+
+  setTimeout(()=>{
+    
+      if(!e.target.classList.contains("bloomed")){
+        e.target.classList.add("bloomed")
+      }
+
+    }, 1000)
+  
+}
+
+
+
   return (
-    <div className="App">
+    <div className="App" >
       <div id="viewport">
+        <img id="bonsai-img" className="pixelart" alt="" src={bonsai} />
         <div id="hunter">
             <img className="hd_sheet pixelart" alt="" src={hunter_down_sheet} />
         </div>
 
+        <div id="fp-one" className="flowerpot " >
+          <img className="bloomin_sheet pixelart bloomed" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-two" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-three" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-four" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-five" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-six" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-seven" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-eight" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-nine" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-ten" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+ 
+        <div id="fp-eleven" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="fp-twelve" className="flowerpot " >
+          <img className="bloomin_sheet pixelart" alt="" src = {blooming} onMouseEnter={(e)=> startBlooming(e)}/>
+        </div>
+
+        <div id="logo-shine">
+          <img id="logo-shine-sheet" className="pixelart" alt="" src={ar_logo_shine} />
+        </div>
+  
       </div>
     </div>
   );
